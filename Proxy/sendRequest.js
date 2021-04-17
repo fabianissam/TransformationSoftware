f8731478d2f907b399798066926a4e93eb8eae6e
Fabian Abdallah <fabian.abdallah98@googlemail.com>
2021-04-17 17:37:36 +0200

    added feature for rest client sending graphql queries instead of rest queies

--- Proxy/sendRequest.js
+++ Proxy/sendRequest.js
@@ -12,24 +12,36 @@ module.exports = function sendRequest(
     body: {
       query: query,
     },
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((json) => {
-      console.log(json);
-      if (basicAuth) {
-        if (method === "get") {
-          return json.data[`viewer${basicAuthName}`][operation];
-        } else {
-          return json.data[`mutationViewer${basicAuthName}`][operation];
-        }
-      } else {
-        return json.data[operation];
-      }
-    })
-    .catch((err) => {
-      return err;
+  }).then((res) => {
+    return res.json();
+  });
+// when rest client makes normal graphql queries
+  if (
+    operation === undefined &&
+    basicAuth === undefined &&
+    basicAuthName === basicAuthName &&
+    method === undefined
+  ) {
+    result.then((json) => {
+      return json;
     });
+  } else { // when client send normal rest queries
+    result
+      .then((json) => {
+        console.log(json);
+        if (basicAuth) {
+          if (method === "get") {
+            return json.data[`viewer${basicAuthName}`][operation];
+          } else {
+            return json.data[`mutationViewer${basicAuthName}`][operation];
+          }
+        } else {
+          return json.data[operation];
+        }
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
   return result;
 };
